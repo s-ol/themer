9c95732d5c82c8b8094a12c298886cef816d1c46
s-ol <s-ol@users.noreply.github.com>
2017-04-10 21:50:17 +0200

    Remove system-wide files from setup.py
    Release 1.8.1

--- setup.py
+++ setup.py
@@ -2,10 +2,8 @@
 # vim:fileencoding=utf-8:noet
 from __future__ import unicode_literals
 import os
-import sys
 
 from setuptools import setup, find_packages
-from setuptools.command.install import install
 
 here = os.path.abspath(os.path.dirname(__file__))
 try:
@@ -13,30 +11,18 @@ try:
 except IOError:
     README = ''
 
-class new_install(install):
-    def run(self):
-        print( self.root )
-        install.run(self) # invoke original install
-
-        root = self.root or '/'
-        self.mkpath(os.path.join(root, 'usr/share/themer'))
-        self.mkpath(os.path.join(root, 'usr/share/fish/completions'))
-        self.copy_tree('data/default', os.path.join(root, 'usr/share/themer/default'))
-        self.copy_file('data/fish/themer.fish', os.path.join(root, 'usr/share/fish/completions/'))
-
 setup(
     name='Themer',
-    version='1.8',
+    version='1.8.1',
     description='Themer is a colorscheme generator and manager for your desktop.',
     long_description=README,
     author='Charles Leifer, Sol Bekic',
-    author_email='s0lll0s@blinkenshell.org',
+    author_email='s+py@s-ol.nu',
     url='https://github.com/s-ol/themer',
     scripts=['scripts/themer'],
     license='MIT',
     keywords='wm colorscheme color theme wallpaper',
     packages=find_packages(exclude=('tests', 'tests.*')),
     include_package_data=True,
-    install_requires=['pyyaml','jinja2','pillow', 'requests'],
-    cmdclass=dict(install=new_install)
+    install_requires=['pyyaml', 'jinja2', 'pillow', 'requests'],
 )
